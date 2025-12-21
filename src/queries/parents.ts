7eacc9493b32ae386eab15a13737c074681b3d62
Tofik Khan <tofik.khan@mkausa.org>
2025-12-21 15:41:38 -0800

    Create Parents Queries

---
+++ src/queries/parents.ts
@@ -0,0 +1,55 @@
+import { API } from "@/api";
+import { User } from "@/types/user";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useParentsQuery = () => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  return useQuery({
+    queryKey: ["parents"],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently();
+      return API.getParents({ authToken: token });
+    },
+    enabled: !!isAuthenticated,
+    select: (response) => response.data,
+  });
+};
+
+export const useOneParentsQuery = (id) => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  return useQuery({
+    queryKey: ["parent", id],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently();
+      return API.getOneParent({ authToken: token, id });
+    },
+    enabled: !!isAuthenticated,
+    select: (response) => response.data,
+  });
+};
+
+export const useUpdateParentMutation = ({ onSuccess, onError }) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async ({ data, id }: { data: User; id: string }) => {
+      const token = await getAccessTokenSilently();
+      return API.updateParent({
+        authToken: token,
+        id,
+        data,
+      });
+    },
+    onSuccess: (params) => {
+      queryClient.invalidateQueries({
+        queryKey: ["parents"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["parent"],
+      });
+      onSuccess(params);
+    },
+    onError,
+  });
+};
